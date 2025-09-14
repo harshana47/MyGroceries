b5affb183bfe583cfbd1013fdbcc97e866c71bca
harshana47 <pabodhaharshana15.com>
2025-09-14 11:29:05 +0530

    fix(babel): use minimal preset to resolve expo-router Babel option error during EAS bundle

--- babel.config.js
+++ babel.config.js
@@ -1,11 +1,7 @@
 module.exports = function (api) {
   api.cache(true);
   return {
-    presets: [["babel-preset-expo", { jsxImportSource: "nativewind" }]],
-    plugins: [
-      "nativewind/babel",
-      // Reanimated v4 moved the Babel plugin to react-native-worklets
-      "react-native-worklets/plugin",
-    ],
+    presets: ['babel-preset-expo'],
+    plugins: [],
   };
 };
